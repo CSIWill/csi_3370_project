9a1bb0f23e3e0dccfb1b434c43d36664720193ae
CSIWill <weng2@oakland.edu>
2023-04-08 22:22:36 -0400

    successful instatiate registration, get id, and update

--- customerclass.py
+++ customerclass.py
@@ -1,11 +1,12 @@
 import mysql.connector
+
 cnx = mysql.connector.connect(
     host = "localhost",
     user = "root",
     password = "csi3370!",
     database ="predatory_elephants"
     )
-cursor = cnx.cursor(buffered=True)
+
 
 class Customer():
     def __init__(self, firstname, lastname, address, phone, email):
@@ -19,15 +20,42 @@ class Customer():
         self.__height = 0
         self.__age = 0
         self.__customer_id = 0
-    def registerCustomer(self):
-        
-        # check to see if customer already exists in database
-        # if not found, commit to database, and change customer_id to primary key value
-        addCustomer = "INSERT INTO customer (customer_fname, customer_lname, customer_phone, customer_email, customer_gender, customer_address, weight, height, age) VALUES (%s, %s, %s, %s, %s, %s, %i, %i, %i)"
-        info = (self.__customer_firstname,self.__customer_lastname, self.__customer_phone, self.__customer_email, self.__customer_gender, self.__customer_address, self.__weight, self.__height, self.__age)
-        cursor.execute(addCustomer,info)
-        cnx.commit()
+    def get_info(self):
+        return (self.__customer_firstname, self.__customer_lastname, self.__customer_phone, self.__customer_email, self.__customer_gender, self.__customer_address, self.__weight, self.__height, self.__age)
+    def contact_info(self):
+        return (self.__customer_firstname, self.__customer_lastname, self.__customer_phone)
+
+    def getCustomerID(self):
+        cursor = cnx.cursor(buffered=True)
+        getIDquery = ("SELECT customer_id FROM customer WHERE customer_fname = %s AND customer_lname = %s AND customer_phone = %s")
+        contact = (self.__customer_firstname,self.__customer_lastname,self.__customer_phone)
+        cursor.execute(getIDquery,contact)
+        results = cursor.fetchall()
+        if len(results) < 1:
+            self.__customer_id = 0
+        else:
+            self.__customer_id = results[0][0]
         cursor.close()
+        # cnx.close()
+        return self.__customer_id
+    def registerCustomer(self):
+        cursor1 = cnx.cursor(buffered=True)
+        # check to see if customer already exists in database
+        
+        if self.getCustomerID() == 0:
+            print("creating customer")
+            addCustomer = "INSERT INTO customer (customer_fname, customer_lname, customer_phone, customer_email, customer_gender, customer_address, weight, height, age) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            info = self.get_info()
+            
+            
+            cursor1.execute(addCustomer,info)
+            cnx.commit()
+        else:
+            print("customer already exists")
+        # if not found, commit to database, and change customer_id to primary key value
+        
+        cursor1.close()
+        # cnx.close()
         return
     def updateCustomer(self, firstname, lastname, address, phone):
         self.__customer_firstname = firstname
@@ -40,33 +68,23 @@ class Customer():
         # delete entry in database
         return
     def updateInfoGoals(self, gender='NA', weight=0, height=0, age=0):
-        cursor = cnx.cursor(buffered=True)
+        cursor2 = cnx.cursor(buffered=True)
         self.__customer_gender = gender
         self.__weight = weight
         self.__height = height
         self.__age = age
-        getIDquery = ("SELECT customer_id FROM customer WHERE customer_fname = %s AND customer_lname = %s AND customer_phone = %s")
-        customerContact = (self.__customer_firstname, self.__customer_lastname, self.__customer_phone)
-        cursor.execute(getIDquery,customerContact)
-        results = cursor.fetchall()
-        for result in results:
-            customer_id = result[0]
         updateBodyInfo = ("UPDATE customer "
-                          "SET gender = %s, "
-                          "weight = %i, "
-                          "height = %i, "
-                          "age = %i "
-                          "WHERE customer_id = %i")
-        bodyInfo = (self.__customer_gender,self.__weight,self.__height,self.__age, customer_id)
-        cursor.execute(updateBodyInfo,bodyInfo)
+                          "SET customer_gender = %s, "
+                          "weight = %s, "
+                          "height = %s, "
+                          "age = %s "
+                          "WHERE customer_id = %s")
+        self.getCustomerID()
+        bodyInfo = (self.__customer_gender,self.__weight,self.__height,self.__age, self.__customer_id)
+        cursor2.execute(updateBodyInfo,bodyInfo)
         cnx.commit()
-        cursor.close()
+        cursor2.close()
         return
-    def get_info(self):
-        
-        return (self.__customer_firstname, self.__customer_lastname, self.__customer_phone, self.__customer_email, self.__customer_gender, self.__customer_address, self.__weight, self.__height, self.__age)
-    def contact_info(self):
-        return (self.__customer_firstname, self.__customer_lastname, self.__customer_phone)
     def requestRoutine(self):
         return
     def addRoutine(self):
@@ -75,5 +93,6 @@ class Customer():
         return
     def trackWorkout(self):
         return
-    cnx.close()
-        
+    def closecnx(self):
+        cnx.close()
+        return
--- database.py
+++ database.py
@@ -31,44 +31,51 @@
 #userInfo.registerCustomer()
 
 
-import mysql.connector
+# import mysql.connector
 from customerclass import Customer
-cnx = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "csi3370!",
-    database ="predatory_elephants"
-)
-print(cnx)
-cursor = cnx.cursor(buffered=True)
+# cnx = mysql.connector.connect(
+#     host = "localhost",
+#     user = "root",
+#     password = "csi3370!",
+#     database ="predatory_elephants"
+# )
+# print(cnx)
+# cursor = cnx.cursor(buffered=True)
 #input from screen
 myInput = ['muffin','man', '123 Blueberry lane','1234567890','muffin@bakery.com']
 # instantiate class
 myCustomer = Customer(myInput[0],myInput[1],myInput[2],myInput[3],myInput[4])
 
 #myCustomer.registerCustomer()
-myInfo = myCustomer.get_info()
-addCustomer = "INSERT INTO customer (customer_fname, customer_lname, customer_phone, customer_email, customer_gender, customer_address, weight, height, age) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-cursor.execute(addCustomer, myInfo)
-cnx.commit()
-getIDquery = ("SELECT customer_id FROM customer WHERE customer_fname = %s AND customer_lname = %s AND customer_phone = %s")
-myContact = myCustomer.contact_info()
-cursor.execute(getIDquery,myContact)
-results = cursor.fetchall()
-for result in results:
-    customer_id = result[0]
-updateBodyInfo = ("UPDATE customer "
-                "SET customer_gender = %s, "
-                "weight = %s, "
-                "height = %s, "
-                "age = %s "
-                "WHERE customer_id = %s")
-bodyInfo = ('M',200,72,45, customer_id)
-cursor.execute(updateBodyInfo,bodyInfo)
-cnx.commit()
-        
-cursor.close()
-cnx.close()
+# myInfo = myCustomer.get_info()
+# addCustomer = "INSERT INTO customer (customer_fname, customer_lname, customer_phone, customer_email, customer_gender, customer_address, weight, height, age) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+# cursor.execute(addCustomer, myInfo)
+# cnx.commit()
+# getIDquery = ("SELECT customer_id FROM customer WHERE customer_fname = %s AND customer_lname = %s AND customer_phone = %s")
+# myContact = myCustomer.contact_info()
+# cursor.execute(getIDquery,myContact)
+# results = cursor.fetchall()
+# for result in results:
+#     customer_id = result[0]
+# updateBodyInfo = ("UPDATE customer "
+#                 "SET customer_gender = %s, "
+#                 "weight = %s, "
+#                 "height = %s, "
+#                 "age = %s "
+#                 "WHERE customer_id = %s")
+# bodyInfo = ('M',200,72,50, customer_id)
+# cursor.execute(updateBodyInfo,bodyInfo)
+# cnx.commit()
 
-cursor.close()
-cnx.close()
+# myCustomer.getCustomerID()
+# print(myCustomer.getCustomerID())
+# if myCustomer.getCustomerID() == 0:
+#     print("creating customer")
+# else:
+#     print("customer already exists")
+
+myCustomer.registerCustomer()
+myCustomer.updateInfoGoals('M',155,72,50)
+# cursor.close()
+# cnx.close()
+myCustomer.closecnx()
